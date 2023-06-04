81f4570f919330eab935b2d55071ba14342b4fb2
Yana Bashtyk <yana_bashtyk@epam.com>
2023-06-04 23:51:47 +0400

    feat: create lambda & apigateway configurations, deploy code to aws

---
+++ product-service/lib/product-service-stack.ts
@@ -0,0 +1,58 @@
+import * as cdk from 'aws-cdk-lib';
+import * as apiGateway from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Construct } from 'constructs';
+import * as path from 'path';
+
+export class ProductServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const getProductsList = new NodejsFunction(this, 'getProductsList', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      environment: {
+        PRODUCT_AWS_REGION: process.env.PRODUCT_AWS_REGION!,
+      },
+      entry: path.join(__dirname,'../lambda/getProductsList.ts'),
+      functionName: 'getProductsList',
+      handler: 'productsListHandler'
+    });
+
+    const getProductsById = new NodejsFunction(this, 'getProductsById', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      environment: {
+        PRODUCT_AWS_REGION: process.env.PRODUCT_AWS_REGION!,
+      },
+      entry: path.join(__dirname,'../lambda/getProductsById.ts'),
+      functionName: 'getProductsById',
+      handler: 'productsIdHandler'
+    });
+
+    const httpApi = new apiGateway.HttpApi(this, 'ProductsHttpApi', {
+      apiName: 'Products Http Api',
+      corsPreflight: {
+        allowHeaders: ['*'],
+        allowOrigins: ['*'],
+        allowMethods: [apiGateway.CorsHttpMethod.ANY]
+      }
+    });
+
+    const lambdaIntegration = new HttpLambdaIntegration('productsIntegration', getProductsList);
+    httpApi.addRoutes({
+      path: '/products',
+      methods: [apiGateway.HttpMethod.GET],
+      integration: lambdaIntegration,
+    });
+
+    const getProductsByIdIntegration = new HttpLambdaIntegration('productsByIdIntegration', getProductsById);
+    httpApi.addRoutes(
+      {
+        path: '/products/{productId}',
+        methods: [apiGateway.HttpMethod.GET],
+        integration: getProductsByIdIntegration,
+      }
+    )
+  }
+}

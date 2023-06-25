c3323a1b55ddec8663f44a0dfc0e87e9a1861901
Yana Bashtyk <yankabashtyk16@gmail.com>
2023-06-25 18:16:47 +0400

    feat: create lambda catalogBatchProcess to create product

---
+++ product-service/lambda/catalogBatchProcess.ts
@@ -0,0 +1,41 @@
+import { Handler } from 'aws-lambda';
+import {createProductHandler} from './createProduct';
+import { Context } from 'aws-lambda';
+import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
+const sns = new SNSClient({});
+
+export const catalogBatchProcessHandler: Handler = async (event) => {
+  console.log('catalogBatchProcess', JSON.stringify(event, null, 2));
+
+  try {
+    for (const record of event.Records) {
+      const newProductResponse = await createProductHandler(record, {} as Context, () => {});
+
+      console.log('newProductData', newProductResponse);
+
+      const snsParams = {
+        Subject: 'Product Created',
+        Message: `A new product has been created: ${newProductResponse.body}`,
+        TopicArn: process.env.CREATE_PRODUCT_TOPIC_ARN,
+      };
+      if(newProductResponse.statusCode === 200) {
+        await sns.send(new PublishCommand(snsParams));
+        console.log('SNS event sent:', snsParams);
+      } else {
+        console.log('SNS event not sent:', snsParams);
+      }
+    }
+
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message: event.Records}),
+    };
+  } catch (err: any) {
+    console.error('Error by processing file:', err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: `Error creating product: ${err.message}`}),
+    };
+  }
+}
